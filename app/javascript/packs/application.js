0c6fcd187068f774b91c15f62ef79c2c8c5d9e60
Dan Kim <itsdanya@gmail.com>
2018-08-07 23:19:54 +0300

    Install Tailwind CSS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,13 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
-
+// Initialize Stimulus
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
 
 const application = Application.start()
 const context = require.context("controllers", true, /.js$/)
 application.load(definitionsFromContext(context))
+
+// Styles & Tailwind
+import '../css/application.css'
