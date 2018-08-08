21af2f893a9dece08700249f2393c026c7306fc1
Dan Kim <itsdanya@gmail.com>
2018-08-08 20:22:54 +0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create!(email: 'foo1@bar.com', password: 'qwerty123')
+
+5.times do |i|
+  deck = Deck.create!(name: "Deck #{i}", user: user)
+
+  10.times do |i|
+    deck.cards.create!(front: "Front #{i}", back: "Back #{i}")
+  end
+end
