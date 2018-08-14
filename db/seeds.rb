963d2fd57dc5979181e7790b9a33006c9eea51c9
Dan Kim <itsdanya@gmail.com>
2018-08-15 00:20:44 +0300

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user = User.create!(email: 'foo1@bar.com', password: 'qwerty123')
+user = User.create!(email: 'foo@bar.com', password: 'qwerty123')
 
 5.times do |i|
   deck = Deck.create!(name: "Deck #{i + 1}", user: user)
