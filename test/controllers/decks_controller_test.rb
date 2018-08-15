f2b196118a653b4cd632cec04931ff2125b0732b
Dan Kim <itsdanya@gmail.com>
2018-08-15 17:43:34 +0300

    Add tests for DecksController

--- test/controllers/decks_controller_test.rb
+++ test/controllers/decks_controller_test.rb
@@ -1,7 +1,52 @@
 require 'test_helper'
 
 class DecksControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @deck = decks(:deck)
+    sign_in users(:foo)
+  end
+
+  test 'should get index' do
+    get decks_url
+    assert_response :success
+  end
+
+  test 'should get new' do
+    get new_deck_url
+    assert_response :success
+  end
+
+  test 'should create deck' do
+    assert_difference('Deck.count') do
+      post decks_url, params: { deck: { name: @deck.name } }
+    end
+
+    assert_redirected_to deck_url(Deck.last)
+  end
+
+  test 'should show deck' do
+    get deck_url(@deck)
+    assert_response :success
+  end
+
+  test 'should get edit' do
+    skip
+    get edit_deck_url(@deck)
+    assert_response :success
+  end
+
+  test 'should update deck' do
+    skip
+    patch deck_url(@deck), params: { deck: { name: @deck.name } }
+    assert_redirected_to deck_url(@deck)
+  end
+
+  test 'should destroy deck' do
+    skip
+    assert_difference('Deck.count', -1) do
+      delete deck_url(@deck)
+    end
+
+    assert_redirected_to decks_url
+  end
 end
