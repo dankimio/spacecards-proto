5c8d9c6b90a3ea68044c6dd91697ab59a1d12ff6
Dan Kim <itsdanya@gmail.com>
2018-08-15 22:29:15 +0300

    Order cards by creation date in cards#index

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -3,7 +3,7 @@ class CardsController < ApplicationController
   before_action :set_card, only: %i[update destroy]
 
   def index
-    @cards = @deck.cards
+    @cards = @deck.cards.order(created_at: :desc)
   end
 
   def new
