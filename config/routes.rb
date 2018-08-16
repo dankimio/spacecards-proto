277ede65cda2814583ae04739ca84480a7373964
Dan Kim <itsdanya@gmail.com>
2018-08-16 23:40:56 +0300

    Create index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'decks#index'
+  root 'pages#index'
 
   resources :decks do
     resources :recalls, only: %i[new]
