fc9e3285df8a34c5e9829563b7ee0baeca4fc1fa
Dan Kim <itsdanya@gmail.com>
2018-08-18 02:38:42 +0300

    Card: touch recalled_at, add new scopes

--- app/models/card.rb
+++ app/models/card.rb
@@ -3,6 +3,9 @@ class Card < ApplicationRecord
 
   scope :due, -> { where('due_on <= ? OR due_on IS NULL', Date.today) }
   scope :random, -> { order('RANDOM()') }
+  scope :new_cards, -> { where(recalled_at: nil) }
+  scope :review_cards, -> { where.not(recalled_at: nil) }
+  scope :recalled_today, -> { where('recalled_at > ?', Time.zone.now.beginning_of_day) }
 
   def recall(quality)
     flashcard = Repetition::Flashcard.new(
@@ -15,7 +18,8 @@ class Card < ApplicationRecord
       easiness_factor: flashcard.easiness_factor,
       repetitions: flashcard.repetitions,
       interval: flashcard.interval,
-      due_on: flashcard.due_on
+      due_on: flashcard.due_on,
+      recalled_at: Time.zone.now
     )
   end
 end
