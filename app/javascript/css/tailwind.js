49d665cc1815fd4b62347e7b217f10c665c01396
Dan Kim <itsdanya@gmail.com>
2019-01-13 19:35:47 +0300

    Use Rubik as the default font

--- app/javascript/css/tailwind.js
+++ app/javascript/css/tailwind.js
@@ -194,13 +194,14 @@ module.exports = {
 
   fonts: {
     'sans': [
-      'Ubuntu',
+      'Rubik',
       'system-ui',
       'BlinkMacSystemFont',
       '-apple-system',
       'Segoe UI',
       'Roboto',
       'Oxygen',
+      'Ubuntu',
       'Cantarell',
       'Fira Sans',
       'Droid Sans',
