bf779925b91c6abfeb4401b32eb26639110007aa
Dan Kim <itsdanya@gmail.com>
2019-02-03 13:45:13 +0300

    Add Google auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,13 @@ class User < ApplicationRecord
 
   after_create_commit :add_sample_data
 
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
+
   private
 
   def add_sample_data
